617f3243542a7c9f69a2fd823a56e1d94ad4d527
Si Linsley <silinsley@protonmail.com>
2017-06-10 22:50:54 +0100

    Add project files.

---
+++ RegexCompare/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace Ch04_Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                WriteLine("Enter a regular expression (press ENTER to use the default regex): ");
+                string defaultRegex = @"^\w[a-z]+$";
+                Regex rgx;
+
+                if (ReadKey().Key == ConsoleKey.Enter)
+                {
+                    rgx = new Regex(defaultRegex);
+                }
+                else
+                {
+                    string userRegex = ReadLine();
+                    rgx = new Regex(userRegex);
+                }
+
+                WriteLine($"The regular expression you have chosen is: {rgx}");
+
+                Write("Enter some input: ");
+                string userInput = ReadLine();
+                if (rgx.IsMatch(userInput))
+                {
+                    WriteLine("They match!");
+                }
+                else
+                {
+                    WriteLine("They don't match, try again?");
+                }
+
+                WriteLine("Would you like to play again? Press ESC to end or any other key to try again.");
+                var playAgain = ReadKey();
+                if (playAgain.Key == ConsoleKey.Escape)
+                {
+                    exit = true;
+                }
+
+            } while (exit == false);
+
+            Environment.Exit(0);
+        }
+    }
+}
